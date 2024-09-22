a75ed68e45f337d644df4d9cd701b59e1b9baeff
Jahni <32658397+jahniyoon@users.noreply.github.com>
2024-09-22 21:57:56 +0900

    Jahni [Add] 푸드파워 A 액티브 딜레이 추가

--- Project FG/Assets/0_ProjectFG/Scripts/FoodPower/FoodPowers/FoodPowerA.cs
+++ Project FG/Assets/0_ProjectFG/Scripts/FoodPower/FoodPowers/FoodPowerA.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 
 namespace JH
@@ -12,26 +13,37 @@ namespace JH
     {
         [Header("Skill")]
         [SerializeField] private FoodPowerSkill m_slashSkill;
+        [SerializeField] private float m_activeDelay;
         private FoodPowerSkill m_activeSkill;
 
 
 
+        Quaternion m_direction;
         public override void Active()
+        {
+
+            m_direction = GetDirection();
+            if (m_direction == Quaternion.identity)
+                return;
+
+            base.Active();
+            Invoke(nameof(ActiveSkill), m_activeDelay);
+
+
+
+        }
+
+        private void ActiveSkill()
         {
             Vector3 position = m_casterPosition.position;
 
-            Quaternion direction = GetDirection();
-            if (direction == Quaternion.identity)
-                return;
-
-            m_activeSkill = Instantiate(m_slashSkill.gameObject, position, direction, m_caster.transform).GetComponent<FoodPowerSkill>();
+            m_activeSkill = Instantiate(m_slashSkill.gameObject, position, m_direction, m_caster.transform).GetComponent<FoodPowerSkill>();
             m_activeSkill.SkillInit(m_caster.gameObject, m_casterPosition);
             m_activeSkill.SetFoodPowerData(m_data.GetLevelData(m_powerLevel));
 
             m_activeSkill.ActiveSkill();
-            base.Active();
-
         }
+
         public override void Inactive()
         {
             if (m_activeSkill)
